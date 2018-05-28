812da0ffafe6e41c22c55ffbbd1e65abd5b2cc38
dlukens <lukensdiego@gmail.com>
2018-05-29 00:30:29 +0200

    Made legs deployable

--- objects.py
+++ objects.py
@@ -1,11 +1,10 @@
 import pygame, pymunk, math
 from pygame.color import *
 
+rocket_h = 44
+rocket_w = 5
+
 def begin(space, rocket_mass, screenx, ground_h, ground_w):
-
-    rocket_h = 44
-    rocket_w = 5
-
     class bodies():
         def __init__(self, w, h, mass, posx, posy, color):
             self.posx = posx
@@ -16,21 +15,22 @@ def begin(space, rocket_mass, screenx, ground_h, ground_w):
 
             vertices = [(0, 0), (w, 0), (w, h), (0, h)]
 
-            self.inertia = pymunk.moment_for_poly(mass, vertices)
+            self.inertia = pymunk.moment_for_poly(mass, vertices, (0, 100))
 
             self.body = pymunk.Body(mass, self.inertia)
             self.body.position = posx, posy
             self.body._set_center_of_gravity([w/2, h/2])
 
             self.shape = pymunk.Poly(self.body, vertices)
-            self.shape._set_friction(0.5)
+            self.shape._set_friction(0.4)
             self.shape.color = color
 
             space.add(self.body, self.shape)
 
     rocket = bodies(rocket_w*4, rocket_h*4, rocket_mass, screenx/2, ground_h + rocket_h, THECOLORS['blue'])
-    leg1 = bodies(4, 40, 10000, rocket.posx - rocket.w/2 + 4, rocket.posy - 30 - 25/2, THECOLORS['red'])
-    leg2 = bodies(4, 40, 10000, rocket.posx + rocket.w/2 - 4, rocket.posy - 30 - 25/2, THECOLORS['pink'])
+    leg1 = bodies(4, 42, 10000, rocket.posx - rocket.w/2, rocket.posy - 30 - 25/2, THECOLORS['red'])
+    leg2 = bodies(4, 42, 10000, rocket.posx + rocket.w/2, rocket.posy - 30 - 25/2, THECOLORS['pink'])
+
 
     ground = bodies(ground_w, ground_h, rocket_mass, 0, 0, THECOLORS['red'])
     ground.body._set_moment(pymunk.inf)
@@ -41,14 +41,15 @@ def begin(space, rocket_mass, screenx, ground_h, ground_w):
 
             self.pin = pymunk.constraint.PivotJoint(rocket.body, leg.body, pin_coord, (leg.w/2, 0))
 
-            self.rotary = pymunk.constraint.DampedRotarySpring(leg.body, rocket.body, rotary_angle0, 6e10, 9e8)
+            self.rotary = pymunk.constraint.DampedRotarySpring(leg.body, rocket.body, rotary_angle0, 8e10, 8e8)
             self.rotary.collide_bodies = False
 
             self.limit = pymunk.constraint.RotaryLimitJoint(leg.body, rocket.body, limit_angle0,  limit_angle1)
 
             space.add(self.pin, self.rotary, self.limit)
 
-    joint1 = joints(leg1, (5, 10), math.pi/4 * 3, -math.pi/4 * 3, 0)
-    joint2 = joints(leg2, (15, 10), -math.pi/4 * 3, 0, math.pi/4 * 3)
+    joint1 = joints(leg1, (rocket.w/4, 10), 0, -math.pi/4 * 3, 0)
+    joint2 = joints(leg2, (rocket.w/4 * 3, 10), 0, 0, math.pi/4 * 3)
+
 
     return(rocket, leg1, leg2, ground, joint1, joint2)
