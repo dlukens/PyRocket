18024a2d4d98f5a915e7d88b40aacfb5f2df5f20
dlukens <lukensdiego@gmail.com>
2018-05-29 00:29:42 +0200

    Various fixes and landing zone added to minimap

--- control.py
+++ control.py
@@ -1,15 +1,18 @@
 import pygame, pymunk, math
 
+import ISAcalcmod, objects
+
 from pygame.locals import *
 
 
-def camera(rocket, ground, gravity, rocket_empty_mass, rocket_fuel_mass, ground_w):
+def camera(rocket, ground, gravity, rocket_empty_mass, rocket_fuel_mass, ceiling):
     rocket.body.velocity = (0, 0)
     angle = pymunk.Body._get_angle(rocket.body)
 
-    if pymunk.Body._get_position(ground.body)[1] < 80:  # Avoid weird floating
+    if pymunk.Body._get_position(ground.body)[1] < 30:  # Avoid weird floating
         ground.body.apply_force_at_local_point(
             (0, ground.body._get_mass() * gravity), (0, 0))
+
 
     # Miscellaneous
     rocket_mass = rocket_empty_mass + rocket_fuel_mass
@@ -17,12 +20,20 @@ def camera(rocket, ground, gravity, rocket_empty_mass, rocket_fuel_mass, ground_
     pymunk.Body._set_mass(ground.body, rocket_mass)
 
     rocket_pos = -(pymunk.Body._get_position(ground.body)
-                   [0] + ground_w / 2), -(pymunk.Body._get_position(ground.body)[1] - 30)
+                   [0] + ground.w / 2)/10, -(pymunk.Body._get_position(ground.body)[1] - 30)/10
 
-    return(rocket_pos, rocket_mass, angle)
+    #drag
+    air_density = ISAcalcmod.CALC((rocket_pos[1]*86000)/(ceiling*10))
+
+    rocket_vel = -ground.body._get_velocity()[0]/10, -ground.body._get_velocity()[1]/10
+
+    dragy = 0.5 * 0.5 * air_density * rocket_vel[1]**2 * 100
+    # print(dragy)
+
+    return(rocket_pos, rocket_mass, angle, air_density)
 
 
-def keys(rocket_fuel_mass, engine_massflow, ground, thrust, force_rcs, launch, angle, rocket):
+def keys(space, rocket_fuel_mass, engine_massflow, ground, thrust, force_rcs, launch, angle, rocket, joint1, joint2, leg1, leg2):
     keys = pygame.key.get_pressed()
     if keys[K_w] or launch:
         ground.body.apply_force_at_local_point(
@@ -40,5 +51,14 @@ def keys(rocket_fuel_mass, engine_massflow, ground, thrust, force_rcs, launch, a
 
     if keys[K_SPACE]:
         launch = True
+
+    if keys[K_g]:
+
+        joint1.rotary._set_rest_angle(math.pi/4 * 3)
+        joint2.rotary._set_rest_angle(-math.pi/4 * 3)
+
+
+
+
 
     return(launch, rocket_fuel_mass)
