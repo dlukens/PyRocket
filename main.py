e44234dd17a3eabf1ece0ab55996b4d4aa4f9e02
dlukens <lukensdiego@gmail.com>
2018-05-29 23:59:39 +0200

    Tweaks

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import labels
 import control
 import objects
 import display
+import game
 
 try:
     import pymunk
@@ -31,20 +32,24 @@ time = 0
 space = pymunk.Space()
 draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-ceiling = 220000
+ceiling = 100000
 
 gravity = 900
 
 # ---Ground---
 ground_h = 200
-ground_w = 800000
+ground_w = 400000
 
 ground_img = pygame.image.load('data/grass.jpg').convert_alpha()
-ground_img = pygame.transform.scale(ground_img, (200, ground_h))
+ground_img = pygame.transform.scale(ground_img, (400, ground_h))
+
+lz_offset = 100000
 
 # ---Rocket---
 rocket_fuel_mass = 800000
 rocket_empty_mass = 150000
+
+rocket_start_pos = ground_w/4  - screenx/2
 
 rocket_mass = rocket_empty_mass + rocket_fuel_mass
 
@@ -52,10 +57,10 @@ engine_isp = 400
 engine_massflow = 80
 
 thrust = engine_isp * engine_massflow * gravity * 30
-force_rcs = 6e7
+force_rcs = 2e8
 
 rocket, leg1, leg2, ground, joint1, joint2 = objects.begin(
-    space, rocket_mass, screenx, ground_h, ground_w)
+    space, rocket_mass, screenx, ground_h, ground_w, rocket_start_pos)
 
 
 # ---Main Loop---
@@ -71,20 +76,24 @@ while running:
 
     # ---Control---
     rocket_pos, rocket_mass, angle, air_density = control.camera(
-        rocket, ground, gravity, rocket_empty_mass, rocket_fuel_mass, ceiling)
+        rocket, ground, gravity, rocket_empty_mass, rocket_fuel_mass, ceiling, screenx)
+
 
     launch, rocket_fuel_mass = control.keys(
         space, rocket_fuel_mass, engine_massflow, ground, thrust, force_rcs, launch, angle, rocket, joint1, joint2, leg1, leg2)
 
+    game.logic(rocket, ground, rocket_pos, lz_offset, space)
+
+
     # ---Screen business---
 
     display.graphics(rocket_pos, screen, space, draw_options,
-                     screeny, ground_img, ground, rocket, ceiling)
+                     screeny, ground_img, ground, rocket, ceiling, lz_offset)
 
-    display.GUI(screen, ground, screenx, rocket_pos, ceiling)
+    display.GUI(screen, ground, screenx, rocket_pos, ceiling, rocket_start_pos, lz_offset)
 
     labels.text(screen, rocket, ground, rocket_pos,
-                thrust, gravity, time, air_density)
+                thrust, gravity, time, air_density, rocket_start_pos)
 
     # Flip screen
     pygame.display.update()
