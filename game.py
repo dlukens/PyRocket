39decefe07789d2de4310e363471a9f39b74dfad
dlukens <lukensdiego@gmail.com>
2018-05-31 01:13:54 +0200

    added landing detection

--- game.py
+++ game.py
@@ -1,19 +1,27 @@
-import pymunk
+import pygame, pymunk, math
 
-def logic(rocket, ground, rocket_pos, lz_offset, space):
-    if lz_offset <= rocket_pos[0] <= lz_offset + 20000:
-        pass
+def logic(rocket, ground, rocket_pos, lz_offset, space, angle):
+
+    landed = False
+
+    if math.radians(10) >= angle >= math.radians(-10) and not landed:
+        if rocket_pos[1] <= 100 and lz_offset <= rocket_pos[0] <= lz_offset + 20000:
+            landed = True
+
+    else:
+        landed = False
+
+
 
     def coll_begin(arbiter, space, data):
-        print(arbiter.shapes[0].id, arbiter.shapes[1].id)
         if arbiter.shapes[0].id == 2 and  arbiter.shapes[1].id == 0:
             print('boom')
 
-        if (arbiter.shapes[0].id == 2 or arbiter.shapes[1].id == 2) and (arbiter.shapes[0].id == 1 or arbiter.shapes[1].id == 1):
-            print('landed')
-
         return True
+
 
     handler = space.add_default_collision_handler()
 
     handler.begin = coll_begin
+
+    return landed
